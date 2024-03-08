bc9469a26881cf732a06414419f1e855520afd6a
2miii <114133339+2miii@users.noreply.github.com>
2024-03-08 23:30:55 +0900

    A vs B rendering

--- src/components/B.js
+++ src/components/B.js
@@ -2,20 +2,25 @@ import React from 'react'
 // B 여러 컴포넌트로 나눠넣음
 
 
-const Message = ({ message }) => {
+const Message = React.memo(({ message }) => {
   return <p>{ message }</p>;
   
-}
-const ListItem = ({ post }) => {
+}) 
+
+
+// React.memo ()사용 시 필요한 영역만 사용한다.
+const ListItem = React.memo(({ post }) => {
+  // 렌더링 체크하려면 이렇게 써도 되나 크롬 profiler 사용
+ // console.log('ListItem Component is rendering') 
   return(
     <li key = {post.id}>
       <p>{post.title}</p>
     </li>
   )
-}
+})
 
 
-const List = ({ posts }) => {
+const List = React.memo( ({ posts }) => {
   return (
     <ul>
       {posts.map(post => (
@@ -23,7 +28,7 @@ const List = ({ posts }) => {
       ))}
     </ul>
   )
-}
+})
 
 export const B = ({ message, posts }) => {
   return (
@@ -34,8 +39,6 @@ export const B = ({ message, posts }) => {
     </div>
   )
 }
-
-
 
 
 export default B;
